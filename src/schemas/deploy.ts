554e0b21bb2b739737d0f8da9ebc49c39e74b9b1
Amitoj Singh <35400192+amitojsingh366@users.noreply.github.com>
2021-06-22 02:00:18 +0300

    update schema

--- src/schemas/deploy.ts
+++ src/schemas/deploy.ts
@@ -10,12 +10,12 @@ export function validateDeploySchema(data: any): ValidatedDeploySchema | undefin
     if (typeof data.branch !== "string") return;
     if (typeof data.envName !== "string") return;
 
-    if (data.containers.map((container) => {
+    if (data.containers.map((container: any) => {
         if (!container.dir) return false;
         if (typeof container.dir !== "string") return false;
 
         if (container.envVars) {
-            if (container.envVars.map((envVar) => {
+            if (container.envVars.map((envVar: any) => {
                 if (!envVar.key) return false;
                 if (!envVar.value) return false;
 
